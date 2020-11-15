d82f91f6dd961ced82f2bb1f777026dd15ee14ef
Sachin Kumar <sjun1992@gmail.com>
2020-11-15 13:24:47 +0530

    Added Routing Setup.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { Route } from 'react-router-dom';
 import './App.css';
 import { HomePage } from './pages/homepage/homepage.component';
 
+const HatsPage = () => { 
+  return (
+    <div>
+      <h1> HATS </h1> 
+    </div>
+  );
+};
 
 function App() {
   return (
     <div> 
-      <HomePage />
+      <Route exact path='/' component={HomePage}></Route>
+      <Route path='/hats' component={HatsPage}></Route>
     </div>
   );
 }
