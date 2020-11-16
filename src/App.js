fde5e5458dd6ae859eb34a065d832222277c929b
Sachin Kumar <sjun1992@gmail.com>
2020-11-16 14:54:56 +0530

    Added code to subscribe & un-subscribe from auth.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/header.component';
@@ -5,17 +6,43 @@ import { HomePage } from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage}></Route>
-        <Route exact path='/shop' component={ShopPage}></Route>
-        <Route exact path='/signin' component={SignInAndSignUpPage}></Route>
-      </Switch>
-    </div>
-  );
+import { auth } from './firebase/firebase.utils';
+
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unSubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unSubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage}></Route>
+          <Route exact path='/shop' component={ShopPage}></Route>
+          <Route exact path='/signin' component={SignInAndSignUpPage}></Route>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
