fed2c49750a632f7ec671d69b66e88d47fe39535
Sachin Kumar <sjun1992@gmail.com>
2020-11-22 17:49:23 +0530

    Dis styling in component item component. Also Made data in reducer in such a way that navigation can happen from homepage.

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -4,17 +4,27 @@ import './collection.styles.scss';
 import { connect } from 'react-redux';
 import { selectCollection } from '../../redux/shop/shop.selector';
 
+import CollectionItem from './../../components/collection-item/collection-item.component';
+
 const CollectionPage = ({ collection }) => {
 
-    console.log('collecions: ',collection);
+    console.log('collecions: ', collection);
+
+    const { title, items } = collection;
+
     return (
-        <div className='category'>
-            <h2>Collection Page !!!</h2>
+        <div className='collection-page'>
+            <h2 className='title'>{title}</h2>
+            <div className='items'>
+                {
+                    items.map(item => <CollectionItem key={item.id} item={item} />)
+                }
+            </div>
         </div>
     );
 };
 
-const mapStateToProps=(state, ownProps) => ({
+const mapStateToProps = (state, ownProps) => ({
     collection: selectCollection(ownProps.match.params.collectionId)(state)
 })
 
