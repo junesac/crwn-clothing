668836cdcf0eecedcc2725358b6414972bfffd3f
Sachin Kumar <sjun1992@gmail.com>
2020-11-22 15:49:41 +0530

    Added shoMoved state from shop component to redux.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,25 @@
 import React from 'react';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
-import SHOP_DATA from './shop.data.js';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectShopCollections } from '../../redux/shop/shop.selector.js';
 
-class ShopPage extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            collections: SHOP_DATA
-        };
-    }
+const ShopPage = ({ collections }) => {
 
-    render() {
-        return (
-            <div className="shop-name">
-                {
-                    this.state.collections.map(({id, ...otherCollectionProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    ))
-                }
-            </div>
-        );
-    }
-
+    return (
+        <div className="shop-name">
+            {
+                collections.map(({ id, ...otherCollectionProps }) => (
+                    <CollectionPreview key={id} {...otherCollectionProps} />
+                ))
+            }
+        </div>
+    );
 }
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+    collections: selectShopCollections
+})
+
+export default connect(mapStateToProps)(ShopPage);
